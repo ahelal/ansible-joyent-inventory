d0af41a8dc5437d9c168da1031db1e9ab510f14f
Adham Helal <helal@HelalMac-2.local>
2014-07-04 20:45:15 +0200

    Updated ini and small refactoring

--- joyent.py
+++ joyent.py
@@ -5,7 +5,6 @@ import sys
 import time
 import ConfigParser
 import logging
-import cPickle as pickle
 from datetime import datetime
 
 try:
@@ -13,8 +12,9 @@ try:
 except ImportError:
     import simplejson as json
 
-def fail_stderr(msg):
-    # Print error and dont break ansible by printing an emtpy JSON
+
+def safe_fail_stderr(msg):
+    ## Print error and dont break ansible by printing an emtpy JSON
     print >> sys.stderr, msg
     print json.dumps(json.loads("{ \"\":\"\" }"), indent=4, sort_keys=True)
     sys.exit(1)
@@ -23,25 +23,28 @@ try:
     from smartdc import DataCenter
 except ImportError:
     ## Print error but dont break ansible inventory
-    fail_stderr("Cant import DataCenter. Please install smartdc")
+    safe_fail_stderr("Cant import DataCenter. Please install smartdc")
 
 
 class JoyentInventory(object):
-
     def __init__(self):
-
         self.inventory = {}
         self.get_config()
         self.set_logger()
 
     def get_config(self):
-
         # Read config
         self.config = ConfigParser.SafeConfigParser()
-        for config_filename in [os.path.abspath(sys.argv[0]).rstrip('.py') + '.ini', 'joyent.ini']:
+        my_name = os.path.abspath(sys.argv[0]).rstrip('.py')
+        path_search = [ my_name + '.ini', 'joyent.ini']
+        if os.environ.get('HELPER'):
+            path_search.extend([os.environ.get('HELPER') + "/" + my_name + ".ini", os.environ.get('HELPER') + "/joyent.ini"])
+
+        for config_filename in path_search:
             if os.path.exists(config_filename):
                 self.config.read(config_filename)
                 break
+
         self.cache_smart = self.config.getboolean('cache', 'cache_smart')
         self.cache_expire = self.config.getint('cache', 'cache_expire')
         self.cache_dir = self.config.get('cache', 'cache_dir')
@@ -56,31 +59,30 @@ class JoyentInventory(object):
         if self.config.has_option('defaults', 'debug'):
             self.debug = self.config.getboolean('defaults', 'debug')
 
+        self.debug_file = self.config.get('defaults', 'debug_file')
+
     def set_logger(self):
         #Logging System
         self.logger = logging.getLogger("joyent")
         if self.debug:
             self.logger.setLevel(logging.DEBUG)
-            handler = logging.FileHandler('hello.log')
-            formatter = logging.Formatter('%(asctime)s - %(message)s')
+            handler = logging.FileHandler(self.debug_file)
+            formatter = logging.Formatter('%(asctime)s : %(process)d : %(funcName)s : %(message)s')
             handler.setFormatter(formatter)
             self.logger.addHandler(handler)
-        self.logger.debug("starting joyent inventory script")
+
         self.logger.debug("using the following file for cache %s" % self.cache_file)
         self.logger.debug("cache expire time %d" % self.cache_expire)
         self.logger.debug("using id_rsa: " + self.joyent_secret + " with '" + self.joyent_key_id + "'")
-
 
     def use_smart_cache(self):
         try:
             pid = os.fork()
             if pid > 0:
-                return self.get_cache()
-                # exit first parent
-                ##sys.exit(0)
+                self.get_cache()
+                sys.exit(0)
         except OSError, e:
-            fail_stderr("use_smart_cache: fork #1 failed: %d (%s)" % (e.errno, e.strerror))
-
+            safe_fail_stderr("fork #1 failed: %d (%s)" % (e.errno, e.strerror))
 
         # decouple from parent environment
         os.chdir("/")
@@ -92,10 +94,9 @@ class JoyentInventory(object):
             pid = os.fork()
             if pid > 0:
                 # exit from second parent, print eventual PID before
-                self.logger.debug("use_smart_cache: Daemon PID %d" % pid)
                 sys.exit(0)
         except OSError, e:
-            fail_stderr("use_smart_cache: fork #2 failed: %d (%s)" % (e.errno, e.strerror))
+            safe_fail_stderr("fork #2 failed: %d (%s)" % (e.errno, e.strerror))
 
         ##
         # Redirect standard file descriptors
@@ -104,13 +105,11 @@ class JoyentInventory(object):
             sys.stdout = open('/dev/null', 'w')
             sys.stderr = open('/dev/null', 'w')
         self.build_inv_from_api()
-        self.logger.debug("use_smart_cache:  PID %d is exiting" % pid)
         sys.exit(0)
 
     def check_cache(self):
         ''' Checks if we can server from cache or API call '''
 
-        self.logger.debug("check_cache: my pid is %d" % os.getpid())
         try:
             stats = os.stat(self.cache_file)
         except:
@@ -118,26 +117,23 @@ class JoyentInventory(object):
             return self.build_inv_from_api()
 
         seconds_since_last_modified = (datetime.now() - datetime.fromtimestamp(stats.st_mtime)).total_seconds()
-        self.logger.debug("check_cache: seconds since last modification %d", seconds_since_last_modified)
+        self.logger.debug("seconds since last modification %d", seconds_since_last_modified)
 
         if seconds_since_last_modified < self.cache_expire:
-            self.logger.debug("check_cache: retrieving servers from cache.")
+            self.logger.debug("retrieving servers from cache.")
             return self.get_cache()
         else:
-            self.logger.debug("check_cache: cache expired.")
             if self.cache_smart:
-                self.logger.debug("check_cache: use smart cache.")
+                self.logger.debug("cache expired. use smart cache.")
                 return self.use_smart_cache()
             else:
-                self.logger.debug("check_cache: dont use smart cache.")
+                self.logger.debug("cache expired. dont use smart cache.")
                 return self.build_inv_from_api()
-
 
     def build_inv_from_api(self):
         servers = self.api_get()
         self.inventory["all"] = []
         self.inventory["hosts"] = {}
-        self.logger.debug("build_inv_from_api - initialised inventory: {}".format(self.inventory))
         for server in servers:
             ## Groups Management
             group = server.type
@@ -150,7 +146,6 @@ class JoyentInventory(object):
             self.inventory["all"].append(server.name)
 
             ## hosts Management
-            ## hosts.update({server.name: []})
             if server.public_ips:
                 ssh_connection = server.public_ips[0]
             elif server.private_ips:
@@ -165,9 +160,7 @@ class JoyentInventory(object):
             ##SmartOS python
             if server.type == "smartmachine":
                 self.inventory["hosts"][server.name]["ansible_python_interpreter"] = "/opt/local/bin/python"
-            ##hosts[server.name]["ansible_ssh_user"] = "root"
 
-        self.logger.debug("build_inv_from_api - constructed inventory: {}".format(self.inventory))
         self.save_cache()
 
 
@@ -186,7 +179,7 @@ class JoyentInventory(object):
             with open(self.cache_file, 'r') as f:
                 self.inventory = json.load(f)
         except IOError, e:
-            fail_stderr("save cache IO Error")
+            safe_fail_stderr("save cache IO Error")
 
     def save_cache(self):
         self.logger.debug("Writing to cache... " + self.cache_file)
@@ -194,31 +187,23 @@ class JoyentInventory(object):
             with open(self.cache_file, 'w') as f:
                 json.dump(self.inventory, f)
         except IOError, e:
-            fail_stderr("save cache IO Error")
+            safe_fail_stderr("save cache IO Error")
 
 if __name__ == '__main__':
-
     joyent = JoyentInventory()
 
     ## Command line parser
     if len(sys.argv) == 2 and (sys.argv[1] == '--list'):
         joyent.check_cache()
-        #print json.dumps(joyent.inventory, indent=4)
-        sys.stdout.write(json.dumps(joyent.inventory, indent=4))
-        sys.stdout.flush()
-        ##print json.dumps(get_inventory(), indent=4)
+        print json.dumps(joyent.inventory, indent=4)
     elif len(sys.argv) == 3 and (sys.argv[1] == '--host'):
         joyent.check_cache()
         print json.dumps(joyent.inventory["hosts"][sys.argv[2]], indent=4)
-        #print json.dumps(get_host(sys.argv[2]), indent=4)
     elif len(sys.argv) == 2 and (sys.argv[1] == '--pint'):
         print json.dumps(joyent.build_inv_from_api(), indent=4)
     else:
         print "Usage: %s --list or --host <hostname>" % sys.argv[0]
         sys.exit(1)
-
-    ##sys.stdout.write(str(inventory))
-    ##sys.stdout.flush()
 
     joyent.logger.debug("Exiting...")
     sys.exit(0)
