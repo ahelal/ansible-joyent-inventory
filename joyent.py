585e2c4e14f21018730aa687079b4ff72d4a30fc
Adham Helal <ahelal@users.noreply.github.com>
2015-04-01 14:36:14 +0200

    Enabled cache_enable option

--- joyent.py
+++ joyent.py
@@ -36,7 +36,6 @@ class JoyentInventory(object):
     def __init__(self):
         self.inventory = {}
         self.__get_config__()
-        self.debug = False
         self.pid_file = __DEFAULT_PID_FILE__
         self.tag_ignore = ["provisioner_ver", "provisioner"]
 
@@ -50,8 +49,11 @@ class JoyentInventory(object):
                 self.config.read(config_filename)
                 break
 
+        self.cache_enable = self._get_config('cache_enable', fail_if_not_set=False, default_value="true").lower() \
+                             in ['true', '1', 't', 'y', 'yes', 'yeah']
+
         self.cache_smart = self._get_config('cache_smart', fail_if_not_set=False, default_value="true").lower() \
-                             in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh']
+                             in ['true', '1', 't', 'y', 'yes', 'yeah']
 
         self.cache_expire = int(self._get_config('cache_expire', fail_if_not_set=False, default_value=300))
         self.cache_file = self._get_config('cache_file', fail_if_not_set=False, default_value=__DEFAULT_CACHE_FILE__)
@@ -59,6 +61,7 @@ class JoyentInventory(object):
         self.joyent_secret = self._get_config('auth_key',  fail_if_not_set=False, default_value=__DEFAULT_AUTH_KEY__)
         self.joyent_username = self._get_config('auth_username', fail_if_not_set=True)
         self.joyent_key_name = self._get_config('auth_key_name', fail_if_not_set=True)
+        self.debug = self._get_config('debug', fail_if_not_set=False)
         # Compile key id
         self.joyent_key_id = "/" + self.joyent_username + "/keys/" + self.joyent_key_name
 
@@ -86,7 +89,7 @@ class JoyentInventory(object):
             return self.build_inv_from_api()
 
         seconds_since_last_modified = (datetime.now() - datetime.fromtimestamp(stats.st_mtime)).total_seconds()
-        if seconds_since_last_modified < self.cache_expire:
+        if seconds_since_last_modified < self.cache_expire and self.cache_enable:
             # Get data from cache
             self.read_cache()
         else:
@@ -113,9 +116,11 @@ class JoyentInventory(object):
                     for tag in server.tags:
                         if tag not in self.tag_ignore:
                             groups.append(server.tags[tag])
-            except AttributeError, E:
-                pass
-                #print "error:", E
+            except AttributeError, e:
+                if self.debug:
+                    sys.stderr.write('Error: {}\n'.format(e))
+                else:
+                    pass
 
             for group in groups:
                 if group not in self.inventory:
@@ -179,16 +184,23 @@ class JoyentInventory(object):
         if len(sys.argv) == 2 and (sys.argv[1] == '--list'):
             self.check_cache()
             print json.dumps(self.inventory, indent=4)
-        elif len(sys.argv) == 3 and (sys.argv[1] == '--host'):
+        elif len(sys.argv) == 2 and (sys.argv[1] == '--host'):
             self.check_cache()
             print json.dumps(self.inventory["hosts"][sys.argv[2]], indent=4)
+        elif len(sys.argv) == 2 and (sys.argv[1] == '--debug'):
+            self.check_cache()
+            print "debug=", self.debug
+            print "Groups"
+            for group in self.inventory:
+                print " _ " + group
+            #print json.dumps(self.inventory["hosts"][sys.argv[2]], indent=4)
         else:
             print "Usage: %s --list or --host <hostname>" % sys.argv[0]
             sys.exit(1)
         sys.stdout.flush()
         sys.stderr.flush()
         # Update cache if we are using smart cache
-        if self.cache_smart and not os.path.exists(self.pid_file):
+        if self.cache_enable and self.cache_smart and not os.path.exists(self.pid_file):
             daemon = Daemonize(app="joyent_inv", pid=self.pid_file, action=self.build_inv_from_api)
             daemon.start()
 
